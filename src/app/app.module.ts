0b8731eefb84451903e3cbac8f39b9ecc4a5e9f3
Julia Fong <Juliamzfong@gmail.com>
2020-04-05 17:30:29 -0500

    Add updated user service file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http'
 
 
 import { AppComponent } from './app.component';
 import { ActiveUsersComponent } from './active-users/active-users.component';
 import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+import { CounterService } from './counter.service'
 
 @NgModule({
   declarations: [
@@ -16,8 +18,9 @@ import { InactiveUsersComponent } from './inactive-users/inactive-users.componen
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CounterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
